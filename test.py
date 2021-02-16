7f26bab7952b8247774c1b95cbfb8b8639e09a61
lingfengzhou <lingfengzhou4@163.com>
2021-02-16 16:13:12 +0800

    提交代码

--- test.py
+++ test.py
@@ -1,2 +1,24 @@
 import  selenium
 import  requests
+import  unittest
+
+class TestDome(unittest.TestCase):
+
+    def test_abc(self):
+        print("abc")
+
+    def test_upper(self):
+        self.assert_('foo'.upper(),'foo')
+
+
+    def test_isupper(self):
+        self.assertTrue('Foo'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+
+    def test_split(self):
+        s = 'hello word'
+        self.assertEqual(s.split(),['hello','word'])
+
+        with self.assertRaises(TypeError):
+            s.split(2)
